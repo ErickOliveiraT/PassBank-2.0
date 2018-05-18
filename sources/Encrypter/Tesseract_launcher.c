cb0784deddecc55e11f436b7ae669d9a3740bc09
ErickOliveiraT <38773400+ErickOliveiraT@users.noreply.github.com>
2018-05-17 23:58:29 -0300

    Tesseract_launcher correction

--- sources/Encrypter/Tesseract_launcher.c
+++ sources/Encrypter/Tesseract_launcher.c
@@ -7,11 +7,11 @@
 #define DEC_PASS_DESCRIPTION system("tesseract vr41egX9rYTw6YCpvK18 Pass_Description_enc.dat Pass_Description.dat")
 #define ENC_USERS_LOGINDATABASE system("tesseract vr41egX9rYTw6YCpvK18 Users_LoginDatabase.dat Users_LoginDatabase_enc.dat")
 #define DEC_USERS_LOGINDATABASE system("tesseract vr41egX9rYTw6YCpvK18 Users_LoginDatabase_enc.dat Users_LoginDatabase.dat ")
-#define DEL_USERS_LOGINDATABASE_enc system("del Users_LoginDatabase_enc.dat")
+#define DEL_USERS_LOGINDATABASE_ENC system("del Users_LoginDatabase_enc.dat")
 #define DEL_USERS_LOGINDATABASE system("del Users_LoginDatabase.dat")
 #define ENC_USERS_PASSDATABASE system("tesseract vr41egX9rYTw6YCpvK18 Users_PassDatabase.dat Users_PassDatabase_enc.dat")
 #define DEC_USERS_PASSDATABASE system("tesseract vr41egX9rYTw6YCpvK18 Users_PassDatabase_enc.dat Users_PassDatabase.dat ")
-#define DEL_USERS_PASSDATABASE_enc system("del Users_PassDatabase_enc.dat")
+#define DEL_USERS_PASSDATABASE_ENC system("del Users_PassDatabase_enc.dat")
 #define DEL_USERS_PASSDATABASE system("del Users_PassDatabase.dat")
 #define ENC_LIST system("tesseract vr41egX9rYTw6YCpvK18 list.dat list_enc.dat")
 #define DEC_LIST system("tesseract vr41egX9rYTw6YCpvK18 list_enc.dat list.dat")
@@ -33,7 +33,8 @@ int main()
 	printf("\t Tesseract Launcher\n\n");
 	printf(" 1 - Encript all\n");
 	printf(" 2 - Decript all\n");
-	printf(" 3 - Delete all\n\n");
+	printf(" 3 - Delete encryted\n");
+	printf(" 4 - Delete decryted database\n\n");
 	printf(" Option: ");
 	scanf("%d", &op);
 	
@@ -44,6 +45,11 @@ int main()
 		ENC_USERS_PASSDATABASE;
 		ENC_LIST;
 		ENC_TEMP;
+		DEL_PASS_DESCRIPTION;
+		DEL_USERS_LOGINDATABASE;
+		DEL_USERS_PASSDATABASE;
+		DEL_LIST;
+		DEL_TEMP;
 	}
 	else if (op == 2)
 	{
@@ -52,8 +58,21 @@ int main()
 		DEC_USERS_PASSDATABASE;
 		DEC_LIST;
 		DEC_TEMP;
+		DEL_PASS_DESCRIPTION_ENC;
+		DEL_USERS_LOGINDATABASE_ENC;
+		DEL_USERS_PASSDATABASE_ENC;
+		DEL_LIST_ENC;
+		DEL_TEMP_ENC;
 	}
 	else if (op == 3)
+	{
+		DEL_PASS_DESCRIPTION_ENC;
+		DEL_USERS_LOGINDATABASE_ENC;
+		DEL_USERS_PASSDATABASE_ENC;
+		DEL_LIST_ENC;
+		DEL_TEMP_ENC;
+	}
+	else if (op == 4)
 	{
 		DEL_PASS_DESCRIPTION;
 		DEL_USERS_LOGINDATABASE;
